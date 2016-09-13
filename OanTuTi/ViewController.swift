b95ecfc62d860bf7ffeb2ac8a52f278d5c2a2547
Lee Nguyen <lee_nguyen366@yahoo.com>
2016-09-13 14:11:46 +0700

    Initial Commit

---
+++ OanTuTi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OanTuTi
+//
+//  Created by Lee Nguyen on 9/13/16.
+//  Copyright © 2016 Lee Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
